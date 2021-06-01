48688f856fb236962d98c6b70ced16e037267290
Deivi Pena <dpena@bankingtech.com.ve>
2021-06-01 09:32:10 -0400

    Bugfix

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,33 +5,35 @@ import FileDirectoryProvider from "./providers/file_directory_provider";
 import LocaleProvider from "./providers/locale_provider";
 import { HomeRoute } from "./routes/home.route";
 import { InfoRoute } from "./routes/info.route";
-import EntradaWorker from "./workers/entrada.worker";
-import { TestWorker } from "./workers/test.worker";
 
 export interface ServiceProviderContract {
     boot(): Promise<any>;
 }
 
 export class Boostrap {
+
+    /**
+     * The declared application's service providers.
+     */
+    public providers: (new() => ServiceProviderContract)[] = [
+        FileDirectoryProvider,
+        LocaleProvider,
+        DatabaseProvider
+    ];
+
     /**
      * The declared application's routes. 
      */
     public routes:  (new() => RouteContract)[] = [
         HomeRoute,
         InfoRoute,
+        //TestWorkerRoute,
     ];
 
     /**
      * The declared application's workers. 
      */
     public workers: (new() => WorkerContract)[] = [
-        //TestWorker,
-        EntradaWorker
-    ];
-
-    public providers: (new() => ServiceProviderContract)[] = [
-        FileDirectoryProvider,
-        LocaleProvider,
-        DatabaseProvider
+        // TestWorker
     ];
 }
