5bf32a83b26204eb05c0451de6607ec47184785c
Deivi Pena <dpena@bankingtech.com.ve>
2021-09-21 18:02:26 -0400

    Actualizaciones

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,13 +1,16 @@
 import { ServiceProviderContract } from "./framework/service_provider";
 import { WorkerContract } from "./framework/queue";
 import { RouteContract } from "./framework/router";
+import DatabaseProvider from "./providers/database.provider";
 import { HomeRoute } from "./routes/home.route";
 import { InfoRoute } from "./routes/info.route";
 import LogProvider from "./providers/log.provider";
 import CacheProvider from "./providers/cache.provider";
+import { LogsListRoute } from "./routes/logs_list.route";
 import RouterProvider from "./providers/router.provider";
-import { TasaBCVRoute } from "./routes/tasa_bcv.route";
-import { TasaDolaTodayRoute } from "./routes/tasa_dolartoday.route";
+import { TestWorkerRoute } from "./routes/test_worker.route";
+import { TestWorker } from "./workers/test.worker";
+import { JobsMonitorRoute } from "./routes/jobs_monitor.route";
 
 export class Boostrap {
 
@@ -16,6 +19,7 @@ export class Boostrap {
      */
     public providers: (new() => ServiceProviderContract)[] = [
         LogProvider,
+        DatabaseProvider,
         CacheProvider,
         RouterProvider,
     ];
@@ -26,13 +30,15 @@ export class Boostrap {
     public routes:  (new() => RouteContract)[] = [
         HomeRoute,
         InfoRoute,
-        TasaBCVRoute,
-        TasaDolaTodayRoute,
+        LogsListRoute,
+        TestWorkerRoute,
+        JobsMonitorRoute,
     ];
 
     /**
      * The declared application's workers. 
      */
     public workers: (new() => WorkerContract)[] = [
+        TestWorker
     ];
 }
