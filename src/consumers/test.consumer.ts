dec2d30da9688eecc0681b5856196f0f6e9b7fd8
Deivi Pena <dpena@bankingtech.com.ve>
2022-04-21 13:31:26 -0400

    Bugfix

--- src/consumers/test.consumer.ts
+++ src/consumers/test.consumer.ts
@@ -1,17 +1,14 @@
-import { BaseConsumer, getEnv, Lang, Logger } from "@ant/framework";
+import { BaseConsumer, Lang, Logger } from "@ant/framework";
 import { EachMessagePayload } from "kafkajs";
-import { snakeCase } from "typeorm/util/StringUtils";
 
 export class TestConsumer extends BaseConsumer {
-    public groupId = snakeCase(getEnv("KAFKA_CONSUMER_GROUP_ID", "my-group"));
-    
-    handler(payload: EachMessagePayload): Promise<void> {
+    handler(value: unknown, payload: EachMessagePayload): Promise<void> {
         return new Promise((resolve) => {
 
             Logger.debug(Lang.__("Running test consumer"));
-            Logger.audit(payload.message.value?.toString() || "");
+            Logger.trace(JSON.stringify(value, null, 4));
+            Logger.audit(JSON.stringify(payload, null, 4));
             resolve();
         })
     }
 }
-
--- src/providers/kafka.provider.ts
+++ src/providers/kafka.provider.ts
@@ -1,12 +1,12 @@
-import { getEnv, Lang, logCatchedError, logCatchedException, Logger, ServiceProvider } from "@ant/framework";
-import { Admin, Consumer, Kafka, LogEntry, logLevel, Message, Producer } from "kafkajs";
+import { getEnv, Lang, logCatchedError, logCatchedException, Logger, now, ServiceProvider } from "@ant/framework";
+import { Admin, Consumer, Kafka, LogEntry, logLevel, Message, Producer, ITopicConfig } from "kafkajs";
 import { snakeCase } from "typeorm/util/StringUtils";
 
 export class KafkaFacade {
     public static kafka: Kafka;
     public static producer: Producer;
 
-    public static async produce(topic: string, message: Message[]): Promise<void> {
+    public static produce(topic: string, message: Message[]): Promise<void> {
         return new Promise((resolve, reject) => {
             Logger.debug(`Producing message to topic [${topic}]`);
 
@@ -22,7 +22,7 @@ export class KafkaFacade {
                 resolve();
             }, error => {
                 Logger.error(`Error producing a message to topic [${topic}]`);
-                Logger.error("Message: " + JSON.stringify(message, null, 4));
+                Logger.trace("Message: " + JSON.stringify(message, null, 4));
                 logCatchedError(error);
                 reject(error);
             })
@@ -31,7 +31,45 @@ export class KafkaFacade {
         })
     }
 
-    public static async getConsumer(groupId?: string): Promise<Consumer> {
+    public static stream(topic: string, data: unknown): Promise<void> {
+        return KafkaFacade.produce(topic, [
+            {
+                value: JSON.stringify(KafkaFacade.prepareStream(topic, data))
+            }
+        ]);
+    }
+
+    public static retry(stream: MessageStream): Promise<void> {
+        if (stream.retries >= parseInt(getEnv("KAFKA_MAX_RETRIES", "3"))) {
+            const msg = Lang.__("Maximum retries attempts [{{retries}}] reached on [{{topic}}]", {
+                retries: stream.retries.toString(),
+                topic: stream.topic
+            });
+
+            Logger.error(msg);
+            Logger.trace(JSON.stringify(stream.body, null, 4));
+            throw new Error(msg);
+        } else {
+            stream.retries++;
+
+            return KafkaFacade.produce(stream.topic, [
+                {
+                    value: JSON.stringify(stream)
+                }
+            ])
+        }
+    }
+    
+    protected static prepareStream(topic: string, data: unknown, retries = 0): MessageStream {
+        return {
+            body: data,
+            timestamp: parseInt(now().format('x')),
+            retries: retries,
+            topic: topic,
+        }
+    }
+
+    public static async getConsumer(groupId: string): Promise<Consumer> {
         const consumer =  KafkaFacade.kafka.consumer({
             groupId: groupId || snakeCase(getEnv("KAFKA_DEFAULT_CONSUMER_GROUP_ID")),
             allowAutoTopicCreation: false,
@@ -54,32 +92,40 @@ export class KafkaFacade {
     }
 }
 
+export type MessageStream = {
+    body: any;
+    timestamp: number;
+    retries: number;
+    topic: string;
+}
+
 export function kafkaLogger() {
     return (entry: LogEntry): void => {
+        const body = JSON.stringify(entry, null, 4);
+
         switch (entry.label) {
         case 'ERROR':
-        case 'NOTHING':
             logCatchedError({
                 name: entry.log.namespace,
-                message: JSON.stringify(entry, null, 4),
+                message: body,
                 stack: entry.log.stack
             });
             break;
 
         case 'WARN':
-            Logger.warn(JSON.stringify(entry, null, 4));
+            Logger.warn(body);
             break;
 
         case 'INFO':
-            Logger.info(JSON.stringify(entry, null, 4));
+            Logger.info(body);
             break;
 
         case 'DEBUG':
-            Logger.audit(JSON.stringify(entry, null, 4));
+            Logger.audit(body);
             break;
 
         default:
-            Logger.audit(JSON.stringify(entry, null, 4));
+            Logger.audit(body);
             break;
         }
     }
@@ -87,7 +133,7 @@ export function kafkaLogger() {
 
 export default class KafkaProvider extends ServiceProvider {
     boot(): Promise<void> {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const brokers = getEnv("KAFKA_BROKERS", "localhost:9092");
 
             const kafka = new Kafka({
@@ -96,20 +142,60 @@ export default class KafkaProvider extends ServiceProvider {
                 retry: {
                     retries: parseInt(getEnv("KAFKA_RETRY_TIMES", "3"))
                 },
-                logLevel: logLevel.NOTHING,
-                // logCreator: () => kafkaLogger
+                logLevel: logLevel.ERROR,
+                logCreator: () => kafkaLogger
             });
+
+            const admin = kafka.admin();
+
+            if (getEnv("KAFKA_CREATE_TOPICS_ON_START", "false") == "true") {
+                await admin.connect().then(async () => {
+                    const raw = getEnv("KAFKA_TOPICS", "default_topic").split(",");
+                    const topics: ITopicConfig[] = [];
+        
+                    for (const topic of raw) {
+                        topics.push({
+                            topic: topic,
+                            numPartitions:  parseInt(getEnv("KAFKA_NUM_PARTITIONS", "1"))
+                        });
+                    }
+                    const current = await admin.listTopics();
+    
+                    Logger.debug(`Current topics: ` + JSON.stringify(current, null, 4));
+    
+                    await admin.createTopics({
+                        waitForLeaders: true,
+                        topics: topics
+                    }).then(result => {
+                        if (result) {
+                            Logger.debug(`Topics [${raw}] successfully created.`)
+                        } else {
+                            Logger.debug(`Topics [${raw}] were not created.`)
+                        }
+                    }, error => {
+                        logCatchedException(error)
+                    })
+                        .catch(logCatchedException)
+                    ;
+                }, error => {
+                    Logger.error(`Kafka admin cannot connect to kafka broker(s) [${brokers}].`);
+                    logCatchedException(error);
+                    reject(error);
+                })
+                    .catch(logCatchedException)
+                ;
+            }
 
             const producer = kafka.producer({
                 allowAutoTopicCreation: false,
                 transactionTimeout: 2000
             });
 
-            producer.connect().then(async () => {
+            await producer.connect().then(async () => {
                 KafkaFacade.kafka = kafka;
                 KafkaFacade.producer = producer;
 
-                Logger.info(Lang.__("Kafka producer successfully connected to kafka broker(s) on [{{brokers}}].", {
+                Logger.debug(Lang.__("Kafka producer successfully connected to kafka broker(s) on [{{brokers}}].", {
                     brokers: brokers
                 }));
 
@@ -146,7 +232,7 @@ export default class KafkaProvider extends ServiceProvider {
                 resolve();
             }, error => {
                 Logger.error(`Kafka producer cannot connect to kafka broker(s) [${brokers}].`);
-                logCatchedError(error);
+                logCatchedException(error);
                 reject(error);
             })
                 .catch(logCatchedException)
