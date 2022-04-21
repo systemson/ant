6f495f4655275b1782fb4c7476cb513dcdaa1b6d
Deivi Pena <dpena@bankingtech.com.ve>
2022-04-21 14:20:53 -0400

    Bugfix

--- src/consumers/test.consumer.ts
+++ src/consumers/test.consumer.ts
@@ -2,11 +2,13 @@ import { BaseConsumer, Lang, Logger } from "@ant/framework";
 import { EachMessagePayload } from "kafkajs";
 
 export class TestConsumer extends BaseConsumer {
-    handler(value: unknown, payload: EachMessagePayload): Promise<void> {
+    topic = "default-topic";
+
+    handler(data: unknown, payload: EachMessagePayload): Promise<void> {
         return new Promise((resolve) => {
 
             Logger.debug(Lang.__("Running test consumer"));
-            Logger.trace(JSON.stringify(value, null, 4));
+            Logger.trace(JSON.stringify(data, null, 4));
             Logger.audit(JSON.stringify(payload, null, 4));
             resolve();
         })
