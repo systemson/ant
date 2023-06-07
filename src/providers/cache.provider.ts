3415012ec884ce1a27594bc7b1aa1a53c4b89863
Deivi Peña <dpena@bankingtech.com.ve>
2023-06-07 10:53:09 -0400

    Minor improvements

--- src/providers/cache.provider.ts
+++ src/providers/cache.provider.ts
@@ -4,40 +4,58 @@ import {
     RedisConfigContract,
     RedisChacheDriver,
     CacheFacade,
-    FilesystemChacheDriver
+    FilesystemChacheDriver,
+    Lang,
+    Logger
 } from "@ant/framework";
 
 export default class CacheProvider extends ServiceProvider {
     boot(): Promise<void> {
-        return new Promise((resolve) => {
-            if (getEnv("APP_CACHE_DRIVER") === "redis") {
-                this.initRedisCache();
-            } else if (getEnv("APP_CACHE_DRIVER") === "file") {
-                this.initFileCache();
+        return new Promise((resolve, reject) => {
+            let result: Promise<void>;
+            const driver = getEnv("APP_CACHE_DRIVER");
+
+            if (driver == "redis") {
+                result = this.initRedisCache();
+            } else if (driver == "file") {
+                result = this.initFileCache();
+            } else {
+                throw new Error(Lang.__("Driver [{{driver}}] is invalid.", {
+                    driver: driver,
+                }));
             }
-    
-            resolve();
+
+            Logger.audit(Lang.__("Preparing [{{driver}}] cache driver.", {
+                driver: driver,
+            }));
+
+            result.then(resolve, reject).catch(reject);
+            
+            Logger.audit(Lang.__("Cache driver [{{driver}}] is ready.", {
+                driver: driver,
+            }));
         });
     }
 
-    protected initRedisCache(): void {
+    protected initRedisCache(): Promise<void> {
         const redisConfig: RedisConfigContract = {
             url: getEnv("REDIS_URL"),
             port: parseInt(getEnv("REDIS_PORT", "6379")),
             host: getEnv("REDIS_HOST", "localhost"),
             password: getEnv("REDIS_PASSWORD"),
+            username: getEnv("REDIS_USERNAME"),
         };
 
         const driver = new RedisChacheDriver(redisConfig);
 
-        CacheFacade.setDriver(driver);
+        return CacheFacade.setDriver(driver);
     }
 
-    protected initFileCache(): void {
+    protected initFileCache(): Promise<void> {
         const driver = new FilesystemChacheDriver(
             getEnv("APP_FILE_CACHE_DIR", "./storage/logs")
         );
 
-        CacheFacade.setDriver(driver);
+        return CacheFacade.setDriver(driver);
     }
 }
